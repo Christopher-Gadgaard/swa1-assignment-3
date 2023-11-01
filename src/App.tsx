fcfe2af5ec13e76d1a212b54a593bd4333b099d9
Vlad Lazar <lazar.vlad151@gmail.com>
2023-11-01 14:47:24 +0100

    added scoreboard view

--- src/App.tsx
+++ src/App.tsx
@@ -2,6 +2,7 @@ import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
 import "./App.css";
 import Game from "./pages/game/game";
 import AccountPage from "./pages/account/account";
+import ScorebardPage from "./pages/scoreboard/scoreboard";
 
 function App() {
   return (
@@ -9,6 +10,7 @@ function App() {
       <Routes>
         <Route path="/" element={<Game />} />
         <Route path="/account" element={<AccountPage />} />
+        <Route path="/scoreboard" element={<ScorebardPage />} />
       </Routes>
     </Router>
   );
