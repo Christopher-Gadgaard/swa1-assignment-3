0530f03b0b623a6a0b1385f35f07cc2ba654789f
Vlad Lazar <lazar.vlad151@gmail.com>
2023-11-01 14:36:28 +0100

    account page and topbar ui

--- src/components/topbar/topbar.tsx
+++ src/components/topbar/topbar.tsx
@@ -1,19 +1,22 @@
 import { FunctionComponent } from "react";
 import LogoutIcon from "@mui/icons-material/Logout";
 import PersonIcon from "@mui/icons-material/Person";
+import ExtensionIcon from "@mui/icons-material/Extension";
+import SportsScoreIcon from "@mui/icons-material/SportsScore";
 import "./styles.css";
+import { useNavigate } from "react-router-dom";
 
 const Topbar: FunctionComponent = () => {
+  const navigate = useNavigate();
   return (
     <div className="topbarContainer">
-      <LogoutIcon
+      <ExtensionIcon className="topbarIcon" onClick={() => navigate("/")} />
+      <SportsScoreIcon
         className="topbarIcon"
-        onClick={() => alert("do soemthing")}
+        onClick={() => navigate("/scoreboard")}
       />
-      <PersonIcon
-        className="topbarIcon"
-        onClick={() => alert("do soemthing")}
-      />
+      <LogoutIcon className="topbarIcon" onClick={() => navigate("/login")} />
+      <PersonIcon className="topbarIcon" onClick={() => navigate("/account")} />
     </div>
   );
 };
