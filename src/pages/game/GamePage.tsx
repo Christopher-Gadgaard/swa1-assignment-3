3c49d68e3cbe0a0507d540114618a142e3b70b5a
Vlad Lazar <lazar.vlad151@gmail.com>
2023-11-12 21:39:14 +0100

    some styling and ending game button

--- src/pages/game/GamePage.tsx
+++ src/pages/game/GamePage.tsx
@@ -8,32 +8,61 @@ import { useState } from "react";
 import { useDispatch, useSelector } from "react-redux";
 import { AppDispatch, RootState } from "../../store";
 import { startNewGame } from "../../thunks/gameServerThunks";
-
+import { Button } from "@mui/material";
+import gemQuestLogo from "../../images/GemQuest.png";
 const Game: React.FC = () => {
+  // State to manage whether the game has started
+  const [gameStarted, setGameStarted] = useState(false);
+  const [gameEnded, setGameEnded] = useState(false);
+  const { token } = useSelector((state: RootState) => state.user);
+  const dispatch = useDispatch<AppDispatch>();
 
-   // State to manage whether the game has started
-   const [gameStarted, setGameStarted] = useState(false);
-   const { token } = useSelector((state: RootState) => state.user);
-   const dispatch = useDispatch<AppDispatch>();
-   const handleStartGame = () => {
+  const handleStartGame = () => {
     if (token) {
       dispatch(startNewGame(token));
-       setGameStarted(true);
+      setGameStarted(true);
     } else {
       alert("You must be logged in to start a game!");
-    } 
+    }
+  };
+  const handleEndGame = () => {
+    if (token) {
+      setGameEnded(true);
+    } else {
+      alert("You must be logged in to end a game!");
+    }
   };
 
-   return (
-    <div>
+  return (
+    <div className="gamePage">
       <Topbar />
+      <img
+        src={gemQuestLogo}
+        style={{
+          marginTop: "100px",
+          width: "200px",
+          height: "200px",
+        }}
+      />
       <div className="gameContainer">
         {!gameStarted ? (
           // Display start button if game hasn't started
-          <button onClick={handleStartGame} className="startGameButton">Start Game</button>
+          <button onClick={handleStartGame} className="startGameButton">
+            Start Game
+          </button>
         ) : (
           // Display game component if game has started
-          <GameComponent onGameStart={handleStartGame} />
+          <>
+            <GameComponent onGameStart={handleStartGame} />
+            <Button
+              variant="contained"
+              onClick={() => {
+                alert("end game");
+              }}
+            >
+              End game
+            </Button>
+          </>
         )}
       </div>
     </div>
