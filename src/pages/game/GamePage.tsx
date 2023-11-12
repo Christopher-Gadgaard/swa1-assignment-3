e8a1861c8f06d5496f1c1d8477cdc235e623dcc1
Chris309283 <Christopher.holmelund@gmail.com>
2023-11-12 19:27:14 +0100

    add start button

--- src/pages/game/GamePage.tsx
+++ src/pages/game/GamePage.tsx
@@ -4,13 +4,28 @@ import Topbar from "../../components/topbar/topbar";
 import "./styles.css";
 
 import GameComponent from "../../components/game/GameComponent";
+import { useState } from "react";
 
 const Game: React.FC = () => {
-  return (
+
+   // State to manage whether the game has started
+   const [gameStarted, setGameStarted] = useState(false);
+   
+   const startGame = () => {
+    setGameStarted(true);
+  };
+
+   return (
     <div>
       <Topbar />
       <div className="gameContainer">
-        <GameComponent />
+        {!gameStarted ? (
+          // Display start button if game hasn't started
+          <button onClick={startGame} className="startGameButton">Start Game</button>
+        ) : (
+          // Display game component if game has started
+          <GameComponent />
+        )}
       </div>
     </div>
   );
